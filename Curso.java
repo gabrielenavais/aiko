0f71c28b77ac6f8cada369ec31b6e6aade96e9d6
Gabriele Navais <134792739+gabrielenavais@users.noreply.github.com>
2024-10-24 11:39:14 -0300

    Create Curso.java

---
+++ Curso.java
@@ -0,0 +1,96 @@
+package br.com.aiko.modelo;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="curso")
+@NamedQuery(name="Curso.findAll", query="SELECT c FROM Curso c")
+public class Curso implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String materia;
+
+	@ManyToMany
+	@JoinTable(name="curso_aluno", joinColumns={@JoinColumn(name = "id_curso")}, inverseJoinColumns={@JoinColumn(name = "id_aluno")})
+	private List<Aluno> alunos;
+
+	@OneToOne
+	@JoinColumn(name="id_professor")
+	private Professor professor;
+
+	public Curso() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMateria() {
+		return this.materia;
+	}
+
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+
+	public List<Aluno> getAlunos() {
+		return this.alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+
+	public Professor getProfessor() {
+		return this.professor;
+	}
+
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	
+
+}
