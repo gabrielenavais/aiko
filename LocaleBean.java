cae0749518ba65f28ad24caaf7f91a71a933e248
Gabriele Navais <134792739+gabrielenavais@users.noreply.github.com>
2024-10-24 11:32:05 -0300

    Create LocaleBean.java

---
+++ LocaleBean.java
@@ -0,0 +1,46 @@
+package br.com.aiko.bean;
+
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LocaleBean implements Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	private Locale currentLocale = new Locale("pt", "BR");
+	
+	public void englishLocale(){
+		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+		
+		currentLocale = Locale.US;
+		viewRoot.setLocale(currentLocale);
+	}
+	
+	public void japaneseLocale(){
+		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+		
+		currentLocale = new Locale("ja", "JP");
+		viewRoot.setLocale(currentLocale);
+	}
+	
+	public void portugueseLocale(){
+		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+		
+		currentLocale = new Locale("pt", "BR");
+		viewRoot.setLocale(currentLocale);
+	}
+	
+	public Locale getCurrentLocale(){
+		return currentLocale;
+	}
+
+}
