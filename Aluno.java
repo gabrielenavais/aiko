9d4069c556fc297623a7402acf8d035d9b5f0019
Gabriele Navais <134792739+gabrielenavais@users.noreply.github.com>
2024-10-24 11:38:07 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,105 @@
+package br.com.aiko.modelo;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "aluno")
+@NamedQuery(name = "Aluno.findAll", query = "SELECT a FROM Aluno a")
+public class Aluno implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String cpf;
+
+	private String email;
+
+	private String nome;
+
+	@ManyToMany(mappedBy = "alunos")
+	private List<Curso> cursos;
+
+	public Aluno() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return this.cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Curso> getCursos() {
+		return this.cursos;
+	}
+
+	public void setCursos(List<Curso> cursos) {
+		this.cursos = cursos;
+	}
+
+	public Curso removeCurso(Curso curso) {
+		getCursos().remove(curso);
+		curso.setAlunos(null);
+
+		return curso;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
